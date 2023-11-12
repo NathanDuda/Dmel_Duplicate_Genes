01306c88af897ba4cc1d7b62689d47600d4c445b
NathanDuda <nathanduda02@gmail.com>
2023-11-11 22:55:29 -0500

    14

--- 14_Dmel_Dsim_dups.R
+++ 14_Dmel_Dsim_dups.R
@@ -140,50 +140,30 @@ fly_counts <- plot_freq_dist_colored %>%
   distinct(dsim_dup,x_axis,y_axis)
 
 
-# Creating the bar plot
-ggplot(fly_counts, aes(fill = factor(dsim_dup,levels=c('No_Duplicated_Ortholog','Dsim_Ortholog_Duplicated')), 
+freq_dist <- ggplot(fly_counts, aes(fill = factor(dsim_dup,levels=c('No_Duplicated_Ortholog','Dsim_Ortholog_Duplicated')), 
                        x = x_axis, y = y_axis)) + 
   geom_bar(position = "stack", stat='identity') +
   scale_x_discrete(limits = factor(1:47)) +
   theme_bw() +
   ylab('Frequency') +
   xlab('') +
-  scale_fill_manual(values = c("plum4", "steelblue3"), name = "") +
-  guides(fill = guide_legend(title = "", override.aes = list(fill = c("plum4", "steelblue3"))))
-
-##########
-
-data <- data.frame(category = c("A", "B", "A", "B", "C", "C", "A"))
-
-# Plot using ggplot2
-ggplot(data, aes(x = category)) +
-  geom_bar()
+  scale_fill_manual(values = c("hotpink3", "cornflowerblue"), name = "") +
+  guides(fill = guide_legend(title = "", override.aes = list(fill = c("hotpink3", "cornflowerblue"))))
 
 
-###########
 
+plot_freq_dist_percent <- fly_counts %>%
+  group_by(x_axis) %>%
+  mutate(Percentage = y_axis / sum(y_axis) * 100)
 
-freq_dist <- ggplot(plot_freq_dist_colored, aes(fill = factor(Var2,levels=c('No_Duplicated_Ortholog','Dsim_Ortholog_Duplicated')), y = Freq, x = Var1)) + 
-  geom_bar(position = "stack", stat = "identity") +
-  scale_x_discrete(limits = factor(1:47)) +
-  theme_bw() +
-  ylab('Frequency') +
-  xlab('') +
-  scale_fill_manual(values = c("plum4", "steelblue3"), name = "") +
-  guides(fill = guide_legend(title = "", override.aes = list(fill = c("plum4", "steelblue3"))))
-
-plot_freq_dist_percent <- plot_freq_dist_colored %>%
-  group_by(Var1) %>%
-  mutate(Percentage = Freq / sum(Freq) * 100)
-
-perc <- ggplot(plot_freq_dist_percent, aes(x = Var1, y = Percentage, fill = factor(Var2,levels=c('No_Duplicated_Ortholog','Dsim_Ortholog_Duplicated')))) +
+perc <- ggplot(plot_freq_dist_percent, aes(x = x_axis, y = Percentage, fill = factor(dsim_dup,levels=c('No_Duplicated_Ortholog','Dsim_Ortholog_Duplicated')))) +
   geom_bar(stat = "identity", position = "stack") +
   scale_x_discrete(limits=factor(1:47)) +
   theme_bw() +
   xlab('') +
   ylab('Percentage of \n Duplicates ') +
-  scale_fill_manual(values = c("plum4", "steelblue3"), name = "") +
-  guides(fill = guide_legend(title = "", override.aes = list(fill = c("plum4", "steelblue3"))))
+  scale_fill_manual(values = c("hotpink3", "cornflowerblue"), name = "") +
+  guides(fill = guide_legend(title = "", override.aes = list(fill = c("hotpink3", "cornflowerblue"))))
 
   
 gg <- ggarrange(freq_dist,perc,nrow = 2, ncol = 1,
@@ -200,69 +180,119 @@ ggsave("./Plots/dsim_freq_dist_barplot.jpg", plot = gg, width = 12, height = 6)
 
 
 plot_freq_dist_colored <- dups_orthos[c('dup','fly','dsim_dup','dup_chrom')]
-#plot_freq_dist_colored$dsim_dup <- paste0(plot_freq_dist_colored$dsim_dup,'_',plot_freq_dist_colored$dup_chrom)
-plot_freq_dist_colored <- as.data.frame(table(plot_freq_dist_colored$dup,
-                                              plot_freq_dist_colored$dsim_dup,
-                                              plot_freq_dist_colored$dup_chrom))
-#plot_freq_dist_colored <- plot_freq_dist_colored[!plot_freq_dist_colored$Freq == 0,]
-#plot_freq_dist_colored <- as.data.frame(table(plot_freq_dist_colored$Freq,plot_freq_dist_colored$Var2,plot_freq_dist_colored$Var3))
+
+
+
+all_chrom <- plot_freq_dist_colored %>%
+  group_by(dup) %>%
+  mutate(x_axis = n_distinct(fly)) %>%
+  ungroup() %>%
+  distinct(dup,dsim_dup,dup_chrom, .keep_all = T) %>%
+  group_by(x_axis,dsim_dup,dup_chrom) %>%
+  mutate(y_axis = n()) %>%
+  ungroup() %>%
+  distinct(dsim_dup,dup_chrom,x_axis,y_axis) %>%
+  group_by(dup_chrom,x_axis) %>%
+  mutate(y_axis=sum(y_axis)) %>%
+  ungroup() %>%
+  select(-dsim_dup) %>%
+  distinct() 
+  
+
+
+plot_all_chrom <- 
+  ggplot(all_chrom, aes(x = x_axis, y = y_axis,fill=dup_chrom)) +
+  geom_bar(stat = "identity", position = "fill") +
+  scale_x_discrete(limits=factor(1:47)) +
+  theme_bw() +
+  ylab('Percentage of \n All Duplicates') +
+  xlab('') +
+  scale_y_continuous(breaks = c(0, .25, .50, .75, 1.00), labels = c("0", "25", "50", "75",'100')) +
+  labs(fill = "")
 
 
 
 
 
-dup_w_ortho_chrom <- plot_freq_dist_colored[plot_freq_dist_colored$Var2 =='Dsim_Ortholog_Duplicated',]
-
-
-dup_w_ortho_chrom <- dup_w_ortho_chrom %>%
-  group_by(Var1) %>%
-  mutate(Percentage = Freq / sum(Freq) * 100) %>%
-  mutate(x_axis = sum(Freq)) %>%
-  na.omit()
-dup_w_ortho_chrom <- as.data.frame(table(dup_w_ortho_chrom$Freq,dup_w_ortho_chrom$Var3))
-dup_w_ortho_chrom <- dup_w_ortho_chrom %>%
-  group_by(Var1) %>%
-  mutate(Percentage = Freq / sum(Freq) * 100) 
+dup_w_ortho_chrom <- plot_freq_dist_colored %>%
+  group_by(dup) %>%
+  mutate(x_axis = n_distinct(fly)) %>%
+  ungroup() %>%
+  distinct(dup,dsim_dup,dup_chrom, .keep_all = T) %>%
+  group_by(x_axis,dsim_dup,dup_chrom) %>%
+  mutate(y_axis = n()) %>%
+  ungroup() %>%
+  distinct(dsim_dup,dup_chrom,x_axis,y_axis) %>%
+  filter(dsim_dup=='Dsim_Ortholog_Duplicated')
 
 plot_dup_w_ortho_chrom <- 
-  ggplot(t, aes(x = Var1, y = Percentage,fill=Var2)) +
+  ggplot(dup_w_ortho_chrom, aes(x = x_axis, y = y_axis,fill=dup_chrom)) +
   geom_bar(stat = "identity", position = "fill") +
   scale_x_discrete(limits=factor(1:47)) +
   theme_bw() +
   ylab('Percentage of \n Duplicates with Orthologs') +
   xlab('') +
   scale_y_continuous(breaks = c(0, .25, .50, .75, 1.00), labels = c("0", "25", "50", "75",'100')) +
-  scale_fill_manual(values = c('steelblue1','steelblue3','steelblue4','royalblue4','navy'), name = "")
+  scale_fill_manual(values = c(colorRampPalette(c("#99CCFF", "royalblue4"))(5)), name = "")
 
 
 
 
-plot_dup_NO_ortho_chrom <- plot_freq_dist_colored[plot_freq_dist_colored$Var2 == 'No_Duplicated_Ortholog',]
 
-plot_dup_NO_ortho_chrom <- plot_dup_NO_ortho_chrom %>%
-  group_by(Var1) %>%
-  mutate(Percentage = Freq / sum(Freq) * 100) %>%
-  mutate(x_axis = sum(Freq)) %>%
-  ungroup()
-plot_dup_NO_ortho_chrom <- as.data.frame(table(plot_dup_NO_ortho_chrom$Freq,plot_dup_NO_ortho_chrom$Var3))
-plot_dup_NO_ortho_chrom <- plot_dup_NO_ortho_chrom %>%
-  group_by(Var1) %>%
-  mutate(Percentage = Freq / sum(Freq) * 100) 
+dup_NO_ortho_chrom <- plot_freq_dist_colored %>%
+  group_by(dup) %>%
+  mutate(x_axis = n_distinct(fly)) %>%
+  ungroup() %>%
+  distinct(dup,dsim_dup,dup_chrom, .keep_all = T) %>%
+  group_by(x_axis,dsim_dup,dup_chrom) %>%
+  mutate(y_axis = n()) %>%
+  ungroup() %>%
+  distinct(dsim_dup,dup_chrom,x_axis,y_axis) %>%
+  filter(dsim_dup=='No_Duplicated_Ortholog')
 
-plot_p <- ggplot(plot_dup_NO_ortho_chrom, aes(x = Var1, y = Percentage,fill=Var2)) +
+
+plot_dup_NO_ortho_chrom <- 
+  ggplot(dup_NO_ortho_chrom, aes(x = x_axis, y = y_axis,fill=dup_chrom)) +
   geom_bar(stat = "identity", position = "fill") +
   scale_x_discrete(limits=factor(1:47)) +
   theme_bw() +
   ylab('Percentage of \n Duplicates without Orthologs') +
   xlab('Number of Flies') +
   scale_y_continuous(breaks = c(0, .25, .50, .75, 1.00), labels = c("0", "25", "50", "75",'100')) +
-  scale_fill_manual(values = c('lightpink','hotpink','mediumorchid1','mediumorchid','mediumorchid4'), name = "")
+  scale_fill_manual(values = c(colorRampPalette(c("plum1", "hotpink4"))(5)), name = "")
+
+
+gg <- ggarrange(freq_dist,perc,plot_dup_w_ortho_chrom,plot_dup_NO_ortho_chrom,
+                nrow=4,ncol=1,align='v',labels = c('A','B','C','D'))
+
+
+ggsave("./Plots/dsim_freq_dist_barplots.jpg", plot = gg, width = 13, height = 12)
 
 
 
-gg <- ggarrange(freq_dist,perc,plot_dup_w_ortho_chrom,plot_p,nrow=4,ncol=1,align='v',
-          labels = c('A','B','C','D'))
+##################################################
+
+fly_counts <- fly_counts %>%
+  select(-dsim_dup) %>%
+  group_by(x_axis) %>%
+  mutate(y_axis=sum(y_axis)) %>%
+  ungroup() %>%
+  distinct()
 
 
-ggsave("./Plots/dsim_freq_dist_barplot.jpg", plot = gg, width = 12, height = 12)
-ggsave("./Plots/dsim_freq_dist_barplot.jpg")
+freq <- ggplot(fly_counts, aes(x = x_axis, y = y_axis)) + 
+  geom_bar(position = "stack", stat='identity') +
+  scale_x_discrete(limits = factor(1:47)) +
+  theme_bw() +
+  ylab('Frequency') +
+  xlab('') 
+
+gg <- ggarrange(freq,plot_all_chrom,nrow=2,ncol=1,align='v',labels=c('A','B'),
+          common.legend = T, legend = "right")
+
+ggsave("./Plots/freq_dist_chrom_barplots.jpg", plot = gg, width = 13, height = 6)
+
+
+
+
+
